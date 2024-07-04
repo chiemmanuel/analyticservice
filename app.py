582181316a3bbd3d8db9e6154429b2db10c89275
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-07-05 01:21:22 +0200

    chore: app file modified to integrate all routes

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_restful import Api
+from routes.data_routes import DataFetchResource
+from routes.analysis_routes import DataAnalysisResource, AnalysisDetailResource
+from routes.visualization_routes import VisualizationResource
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 api = Api(app)
 
+api.add_resource(DataFetchResource, '/fetch-data')
+api.add_resource(DataAnalysisResource, '/analyze/<analysis_id>')
+api.add_resource(AnalysisDetailResource, '/analyze/<analysis_id>')
+api.add_resource(VisualizationResource, '/visualize/<analysis_id>')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=app.config['PORT'])
