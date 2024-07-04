fcde99ab67fd64bffad7fd8b9d9f1c2acb27fc59
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-07-04 15:50:08 +0200

    fix: config file and data service modified to create session before fetching data

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ import os
 
 class Config:
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/microservice_db')
+    EMAIL = os.getenv('EMAIL', 'junor.chi-emmanuel-ngu@epita.fr')
     PORT = int(os.getenv('PORT', 5000))  # Default port is 5000 if not specified
+    
--- services/data_service.py
+++ services/data_service.py
@@ -1,9 +1,15 @@
 import requests
+from config import Config
 from models.data_model import DataModel
 
 def fetch_and_store_data(sheet_name, table_name):
     try:
-        url = f"http://localhost:8001/excel_sheets/table/{table_name}?{sheet_name}"
+        email = Config.EMAIL
+        session_url = f"http://localhost:8001/excel_sheets/create_session?email={email}"
+        auth_response = requests.post(session_url)
+        if auth_response.status_code != 200:
+            return None
+        url = f"http://localhost:8001/excel_sheets/table/{table_name}?sheet_name={sheet_name}"
         response = requests.get(url)
         response.raise_for_status()
         
