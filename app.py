0f182af3262a06e5116876bd68a86ae5e52bcf4c
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2024-07-05 17:39:37 +0200

    chore: app file modified to create data and analysis collection on initialization

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from routes.data_routes import DataFetchResource
 from routes.analysis_routes import DataAnalysisResource, AnalysisDetailResource
 from routes.visualization_routes import VisualizationResource
 from config import Config
+from models.data_model import DataModel
+from models.analysis_model import AnalysisModel
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
+
+DataModel.init_app(app)
+AnalysisModel.init_app(app)
 
 api.add_resource(DataFetchResource, '/fetch-data')
 api.add_resource(DataAnalysisResource, '/analyze')
